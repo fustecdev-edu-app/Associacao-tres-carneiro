aa15020e6858a7663e31cf8513a81a943518afea
eduardo bandeira <fustecdev@gmail.com>
2026-03-15 07:28:54 -0300

    Add files via upload

---
+++ segur.js
@@ -0,0 +1,18 @@
+document.onkeydown = function(e) {
+  // Bloqueia F12
+  if (e.keyCode == 123) {
+    return false;
+  }
+  // Bloqueia Ctrl+Shift+I (Windows) ou Cmd+Option+I (Mac)
+  if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+    return false;
+  }
+  // Bloqueia Ctrl+Shift+J (Console)
+  if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+    return false;
+  }
+  // Bloqueia Ctrl+U (Ver Código Fonte)
+  if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+    return false;
+  }
+};
